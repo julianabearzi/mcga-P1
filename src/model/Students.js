f87e025c97f16ac7f5b3604554c83b68788cf7bd
carla2807 <cm90_mdp@hotmail.com>
2021-10-07 12:31:10 -0300

    add student model

---
+++ src/model/Students.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const studentSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  course: {
+    type: String,
+    required: true,
+  },
+
+  turn: {
+    type: String,
+    enum: ['morning', 'afternoon'],
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Students', studentSchema);
