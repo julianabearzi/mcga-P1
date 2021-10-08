e9a16c3640162766f866b254cec264174c18613f
Juliana Bearzi <julietad299@gmail.com>
2021-10-08 00:27:16 -0300

    add updateStudent

--- src/routes/students/index.js
+++ src/routes/students/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const { param } = require('express-validator');
 const studentsController = require('../../controllers/students');
 
 const router = express.Router();
+const { ValidateStudent } = require('../../validators/students');
 
 router.route('/').get(studentsController.getAllStudents);
+router.route('/').post(ValidateStudent, studentsController.createStudent);
+router
+  .route('/:id')
+  .put(
+    param('id').isMongoId(),
+    ValidateStudent,
+    studentsController.updateStudent
+  );
 
 module.exports = router;
