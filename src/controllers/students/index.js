65b81bfdbb01df6ace923e82ad0cab3f166bc6d0
Juliana Bearzi <julietad299@gmail.com>
2021-10-07 22:25:23 -0300

    add createStudent method

--- src/controllers/students/index.js
+++ src/controllers/students/index.js
@@ -11,11 +11,42 @@ const getAllStudents = async (req, res) => {
   } catch (error) {
     return res.status(500).json({
       error: true,
-      msg: `Internal Server Error`,
+      msg: 'Internal Server Error',
+    });
+  }
+};
+
+const createStudent = async (req, res) => {
+  try {
+    if (
+      !req.body.name ||
+      !req.body.lastName ||
+      !req.body.age ||
+      !req.body.course ||
+      !req.body.turn ||
+      !req.body.amount
+    ) {
+      return res.status(400).json({
+        error: true,
+        msg: 'Missing fields to create a student',
+      });
+    }
+    const student = new Students(req.body);
+    const newStudent = await student.save();
+
+    return res.status(201).json({
+      data: newStudent,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      msg: 'Internal Server Error',
     });
   }
 };
 
 module.exports = {
   getAllStudents,
+  createStudent,
 };
--- src/routes/students/index.js
+++ src/routes/students/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const studentsController = require('../../controllers/students');
 
 const router = express.Router();
+const { ValidateStudent } = require('../../validators/students');
 
 router.route('/').get(studentsController.getAllStudents);
+router.route('/').post(ValidateStudent, studentsController.createStudent);
 
 module.exports = router;
