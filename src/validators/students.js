f343b55c07a918dd36b2a044869ba2dcae42794b
Juliana Bearzi <julietad299@gmail.com>
2021-10-12 16:58:59 -0300

    validation improvements

--- src/validators/students.js
+++ src/validators/students.js
@@ -4,25 +4,35 @@ const { validator } = require('../middlewares/validate');
 const ValidateStudent = [
   check('name')
     .isString()
+    .matches(/^[A-Za-z\s]+$/)
     .trim()
     .withMessage('Please enter a valid name')
     .optional(),
   check('lastName')
     .isString()
     .trim()
+    .matches(/^[A-Za-z\s]+$/)
     .withMessage('Please enter a valid last name')
     .optional(),
   check('age')
     .isNumeric()
     .trim()
     .withMessage('Please enter a valid age')
+    .custom((value) => {
+      if (value >= 17) {
+        return true;
+      }
+      throw new Error('Age cannot be less than 17 years');
+    })
     .optional(),
   check('course')
     .isString()
     .trim()
+    .matches(/^[A-Za-z\s]+$/)
     .withMessage('Please enter a valid course')
     .optional(),
   check('turn')
+    .trim()
     .custom((value) => {
       if (value === 'morning' || value === 'afternoon') {
         return true;
